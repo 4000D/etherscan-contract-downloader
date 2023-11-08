1cf2032f12216f50da5114e43e6e596ef17a12bb
Carl Park <dkdkajej@gmail.com>
2023-11-08 20:10:01 +0900

    feat: download multiple contracts with api key as cli option

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Etherscan Contract Downloader
 
-Download source code of verified contracts from Etherscan.
+> Inspired by [amimaro/smart-contract-downloader](https://github.com/amimaro/smart-contract-downloader)
+
+Download source codes of verified contracts from Etherscan.
 
 ## Installation
 
@@ -11,14 +13,16 @@ npm i -g etherscan-contract-downloader
 ## Usage
 
 ```bash
-Usage: etherscan-contract-downloader [options] <networkName> <contractAddress>
+Usage: etherscan-contract-downloader [options] <networkName> <contractAddresses...>
 
 Arguments:
-  networkName      The name of networks: (ethmain, rinkeby, ropsten, kovan, goerli, polygon, polygonTest, bsc, bscTest, arbitrumOne, arbitrumNova, arbitrumGoerli, fantom, fantomTest)
-  contractAddress  Verified contract address to download source code
+  networkName             The name of networks: (ethmain, rinkeby, ropsten, kovan, goerli, polygon, polygonTest, bsc, bscTest, arbitrumOne, arbitrumNova, arbitrumGoerli, fantom, fantomTest)
+  contractAddresses       Verified contract address to download source code
 
 Options:
-  -d, --dir <dir>  Directory to save source code
+  -d, --dir <dir>         Directory to save source code
+  -k, --api-key <apiKey>  Etherscan API Key
+  -h, --help              display help for command
 ```
 
 ## Example
@@ -29,4 +33,8 @@ etherscan-contract-downloader ethmain 0x1F98431c8aD98523631AE4a59f267346ea31F984
 
 # uniswap v3 universal router
 etherscan-contract-downloader ethmain 0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD --dir ~/uniswap-v3
+
+# gains network contracts
+etherscan-contract-downloader arbitrumOne 0x7edDE7e5900633F698EaB0Dbc97DE640fC5dC015 0x30d8C505516Ab7693e2DE491bdceB028d8ae7EbF 0xa3c13A8FBf49679df032d5cE8eFf2e46b3b8f560 0x4899FEfbf0a5C9881F1DffE6D13f758257298121 0x298a695906e16aea0a184a2815a76ead1a0b7522 0xde5750071CacA8db173FC6543D23d0BCACACFEC3 0xA03e32a42C75FDdDc9a39973ddd082F147393154 0x2e44a81701a8355e59b3204b4a9fe8fc43cbe0c3 0xd85E038593d7A098614721EaE955EC2022B9B91B  0x990BA9Edd8a9615A23E4c452E63A80e519A4a23D  0x673cf5AB7b44Caac43C80dE5b99A37Ed5B3E4Cc6  0x1759eD3BB9ecd661ebEcF4264f7677E09206ACE2  0xAA379DD7Ec0bae467490e89bB2055A7e01231b8f  0x04a5e3cf21B0080B72fAcDca634349A56982D497 0xf67Df2a4339eC1591615d94599081Dd037960d4b 0xcFa6ebD475d89dB04cAd5A756fff1cb2BC5bE33c --dir ~/gains-network
+
 ```
--- src/networks.ts
+++ src/networks.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import { config } from "dotenv";
+config();
 
 // https://github.com/amimaro/smart-contract-downloader/blob/main/networks.ts
 export const NETWORKS = {
@@ -6,98 +7,126 @@ export const NETWORKS = {
     label: "Ethereum Mainnet",
     url: "https://etherscan.io",
     apiKey: process.env.APIKEY_ETHERSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ETHERSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ETHERSCAN ?? apiKey
+      }`,
   },
   rinkeby: {
     label: "Rinkeby Testnet",
     url: "https://rinkeby.etherscan.io",
     apiKey: process.env.APIKEY_ETHERSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-rinkeby.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ETHERSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-rinkeby.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ETHERSCAN ?? apiKey
+      }`,
   },
   ropsten: {
     label: "Ropsten Testnet",
     url: "https://ropsten.etherscan.io",
     apiKey: process.env.APIKEY_ETHERSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-ropsten.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ETHERSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-ropsten.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ETHERSCAN ?? apiKey
+      }`,
   },
   kovan: {
     label: "Kovan Testnet",
     url: "https://kovan.etherscan.io",
     apiKey: process.env.APIKEY_ETHERSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-kovan.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ETHERSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-kovan.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ETHERSCAN ?? apiKey
+      }`,
   },
   goerli: {
     label: "Goerli Testnet",
     url: "https://goerli.etherscan.io",
     apiKey: process.env.APIKEY_ETHERSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-goerli.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ETHERSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-goerli.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ETHERSCAN ?? apiKey
+      }`,
   },
   polygon: {
     label: "Polygon Mainnet",
     url: "https://polygonscan.com",
     apiKey: process.env.APIKEY_POLYGONSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_POLYGONSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api.polygonscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_POLYGONSCAN ?? apiKey
+      }`,
   },
   polygonTest: {
     label: "Polygon Mumbai Testnet",
     url: "https://mumbai.polygonscan.com",
     apiKey: process.env.APIKEY_POLYGONSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-testnet.polygonscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_POLYGONSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-testnet.polygonscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_POLYGONSCAN ?? apiKey
+      }`,
   },
   bsc: {
     label: "Binance Smart Chain Mainnet",
     url: "https://bscscan.com",
     apiKey: process.env.APIKEY_BSCSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api.bscscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_BSCSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api.bscscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_BSCSCAN ?? apiKey
+      }`,
   },
   bscTest: {
     label: "Binance Smart Chain Testnet",
     url: "https://testnet.bscscan.com",
     apiKey: process.env.APIKEY_BSCSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-testnet.bscscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_BSCSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-testnet.bscscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_BSCSCAN ?? apiKey
+      }`,
   },
   arbitrumOne: {
     label: "Arbitrum One Mainnet",
     url: "https://arbiscan.io",
     apiKey: process.env.APIKEY_BSCSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api.arbiscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_BSCSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api.arbiscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ARBITRUMSCAN ?? apiKey
+      }`,
   },
   arbitrumNova: {
     label: "Arbitrum Nova Mainnet",
     url: "https://nova.arbiscan.io",
     apiKey: process.env.APIKEY_ARBITRUMSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-nova.arbiscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ARBITRUMSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-nova.arbiscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ARBITRUMSCAN ?? apiKey
+      }`,
   },
   arbitrumGoerli: {
     label: "Arbitrum Goerli Testnet",
     url: "https://goerli.arbiscan.io",
     apiKey: process.env.APIKEY_ARBITRUMSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-goerli.arbiscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_ARBITRUMSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-goerli.arbiscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_ARBITRUMSCAN ?? apiKey
+      }`,
   },
   fantom: {
     label: "Fantom Mainnet",
     url: "https://ftmscan.com",
     apiKey: process.env.APIKEY_FTMSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api.ftmscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_FTMSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api.ftmscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_FTMSCAN ?? apiKey
+      }`,
   },
   fantomTest: {
     label: "Fantom Testnet",
     url: "https://testnet.ftmscan.com",
     apiKey: process.env.APIKEY_FTMSCAN as string,
-    endpoint: (contractAddress: string) =>
-      `https://api-testnet.ftmscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.APIKEY_FTMSCAN}`,
+    endpoint: (contractAddress: string, apiKey?: string) =>
+      `https://api-testnet.ftmscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${
+        process.env.APIKEY_FTMSCAN ?? apiKey
+      }`,
   },
 };
